8a3ddfeb63853b11737555705566ab7d9df99884
DrDreo <code@anderas-hahn.at>
2023-05-01 20:53:04 +0200

    fix feed format

--- feed.ts
+++ feed.ts
@@ -5,14 +5,13 @@ type FeedEntry = {
     mainText: string; // The text that Alexa reads to the customer. For audio items, this element is ignored, and can contain an empty string ("").
     redirectionUrl: string; // 	Provides the URL target for the Read More link in the Alexa app.
 }
-
 export function getFeedFromJira(issues: AssignedIssues): FeedEntry {
 
     const titleText = `You have ${issues.total} JIRA tickets.`;
     const mainText = titleText + " " + issues.issues.map(issue => issue.title).join(", ");
     return {
-        uid: "urn:uuid:1335c695-cfb8-4ebb-abbd-80da344efa6b",
-        updateDate: Date.now(),
+        uid: crypto.randomUUID(),
+        updateDate: new Date().toISOString(),
         titleText,
         mainText,
         redirectionUrl: "https://www.example.com/path/to/resource/"
